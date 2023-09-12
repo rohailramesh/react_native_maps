8de8e08cfa94d64c913aa5dff10a97d1d6330a6d
Rohail Ramesh <rohailramesh@hotmail.com>
2023-09-12 14:41:08 +0100

    Update App.js

--- MapsApp/App.js
+++ MapsApp/App.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-// import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import MapView, { Callout, Circle, Marker } from "react-native-maps";
 
 export default function App() {
@@ -8,49 +7,9 @@ export default function App() {
     latitude: 51.50611,
     longitude: -0.414997,
   });
-  // const [region, setRegion] = React.useState({
-  //   latitude: 37.78825,
-  //   longitude: -122.4324,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // });
 
   return (
-    <View style={{ marginTop: 50, flex: 1 }}>
-      {/* <GooglePlacesAutocomplete
-        placeholder="Search"
-        fetchDetails={true}
-        GooglePlacesSearchQuery={{
-          rankby: "distance",
-        }}
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          console.log(data, details);
-          setRegion({
-            latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          });
-        }}
-        query={{
-          key: "KEY",
-          language: "en",
-          components: "country:us",
-          types: "establishment",
-          radius: 30000,
-          location: `${region.latitude}, ${region.longitude}`,
-        }}
-        styles={{
-          container: {
-            flex: 0,
-            position: "absolute",
-            width: "100%",
-            zIndex: 1,
-          },
-          listView: { backgroundColor: "white" },
-        }}
-      /> */}
+    <View style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={{
@@ -59,7 +18,7 @@ export default function App() {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        provider="google"
+        // provider="google"
       >
         <Marker
           coordinate={pin}
